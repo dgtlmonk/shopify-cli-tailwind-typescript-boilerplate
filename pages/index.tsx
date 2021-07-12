802457eb803a0b79bcd41dbefe1e1663685e6653
Joel Pablo <dgtlmonk@gmail.com>
2021-07-13 07:01:01 +0800

    chore: setup base tailwindcss (#2)

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import { Heading, Page } from "@shopify/polaris";
 const Index = () => (
   <Page>
     <Heading>Shopify app with Node and React 🎉</Heading>
-    oi ts whoot!
+    <button className="rounded-md p-4 border bg-gray-200 hover:bg-gray-800 hover:text-white">
+      tailwind button
+    </button>
   </Page>
 );
 
